bd88e81a1b59309c3892c131544e572fe52632ad
Shichen Lu <shichenl2@gmail.com>
2021-09-28 20:58:36 -0400

    Add Play/Pause Button to Dash
    Fix some bugs with dash not displaying ip
    Update callbacks to account for a play/pause button controlling the slider

--- python/vis/boreas_plotly.py
+++ python/vis/boreas_plotly.py
@@ -3,7 +3,7 @@ import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
 
@@ -26,7 +26,7 @@ class BoreasPlotly:
         lidar_points = lidar_frame.load_data()
         # Draw lidar points
         pcd_i = np.matmul(C_iv[0:2, 0:2].reshape(1, 2, 2), lidar_points[:, 0:2].reshape(lidar_points.shape[0], 2, 1)).squeeze(-1)
-        rand_idx = np.random.choice(pcd_i.shape[0], size=int(pcd_i.shape[0] * 0.5), replace=False)
+        rand_idx = np.random.choice(pcd_i.shape[0], size=int(pcd_i.shape[0]*0.3), replace=False)
         return pcd_i[rand_idx, 0], pcd_i[rand_idx, 1], lidar_frame, C_i_enu
 
     def visualize(self, frame_idx):
@@ -35,22 +35,28 @@ class BoreasPlotly:
         app.layout = html.Div([
             dcc.Graph(id='graph-with-slider'),
             dcc.Slider(
-                id='timestep-slider',
-                min=0,
-                max=len(self.seq.lidar_frames),
-                value=0,
-                marks={str(idx): str(idx) for idx in range(0, len(self.seq.lidar_frames), 5)},
-                step=1
-            )
+                    id='timestep-slider',
+                    min=0,
+                    max=len(self.seq.lidar_frames),
+                    value=0,
+                    marks={str(idx): str(idx) for idx in range(0, len(self.seq.lidar_frames), 5)},
+                    step=1
+                    ),
+            dcc.Interval(id='animator',
+                         interval=500,  # in milliseconds
+                         n_intervals=0,
+                         disabled=True,
+                         ),
+            html.Button("Play/Pause", id="play_btn")
         ])
 
         @app.callback(
             Output('graph-with-slider', 'figure'),
-            Input('timestep-slider', 'value'))
+            Input('timestep-slider', 'value')
+        )
         def update_figure(idx):
             pcd_x, pcd_y, lidar_scan, C_i_enu = self.get_pcd(idx)
             fig = go.Figure()
-            # fig = px.scatter(x=pcd_x, y=pcd_y)
             map_utils.draw_map_plotly("/home/shichen/datasets/boreas_mini/boreas_lane.osm", fig, lidar_scan.pose[0, 3], lidar_scan.pose[1, 3], C_i_enu, utm=True)
             fig.add_trace(
                 go.Scattergl(x=pcd_x, y=pcd_y, mode='markers', visible=True, marker_size=0.5, marker_color='blue')
@@ -67,5 +73,26 @@ class BoreasPlotly:
             fig.update_layout(showlegend=False)
 
             return fig
+
+        @app.callback(
+            Output('timestep-slider', 'value'),
+            Input('animator', 'n_intervals'),
+            State('timestep-slider', 'value'),
+        )
+        def on_click(n_intervals, slider_idx):
+            if n_intervals is None:
+                return 0
+            else:
+                return slider_idx + 1
+
+        @app.callback(
+            Output('animator', 'disabled'),
+            Input('play_btn', 'n_clicks'),
+            State('animator', 'disabled')
+        )
+        def toggle(n, playing):
+            if n:
+                return not playing
+            return playing
 
         app.run_server(debug=True)
