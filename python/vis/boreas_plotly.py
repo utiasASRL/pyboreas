cbb54846cb35653210697d7eda57a52447aa57cd
Shichen Lu <shichenl2@gmail.com>
2021-10-26 21:48:32 -0400

    Separate Plotly Update Functions for Each Plot, Add Colored Lidar

--- python/vis/boreas_plotly.py
+++ python/vis/boreas_plotly.py
@@ -42,7 +42,7 @@ class BoreasPlotly:
         return camera_image
 
     def get_cam_path(self, idx):
-        cam = self.seq.get_camera(idx)
+        cam = self.camera_frames[idx]
         return cam.path
 
     def get_radar(self, idx, grid_size, grid_res):
@@ -64,16 +64,16 @@ class BoreasPlotly:
             dcc.Graph(id='persp_plot')], style={'display': 'inline-block'}),
 
             html.Div(children=[
+            dcc.Graph(id='radar_plot')], style={'display': 'inline-block'}),
+
+            html.Div(children=[
             dcc.Graph(id='color_lidar_plot')], style={'display': 'inline-block'}),
 
-            # html.Div(children=[
-            # dcc.Graph(id='graph-with-slider4')], style={'display': 'inline-block'}),
-
             dcc.Slider(
-                    id='timestep-slider',
+                    id='timestep_slider',
                     min=0,
                     max=len(self.seq.lidar_frames),
-                    value=0,
+                    value=max(0, min(frame_idx, len(self.seq.lidar_frames))),
                     marks={str(idx): str(idx) for idx in range(0, len(self.seq.lidar_frames), 5)},
                     step=1
                     ),
@@ -89,9 +89,7 @@ class BoreasPlotly:
 
         @app.callback(
             Output('bev_plot', 'figure'),
-            Output('persp_plot', 'figure'),
-            Output('color_lidar_plot', 'figure'),
-            Input('timestep-slider', 'value')
+            Input('timestep_slider', 'value')
         )
         def update_figure(idx):
             pcd, lidar_scan, C_a_enu, C_a_l = self.get_pcd(idx, 0.5)
@@ -99,7 +97,7 @@ class BoreasPlotly:
             # BEV
             pcd_a = np.matmul(C_a_l[0:2, 0:2].reshape(1, 2, 2), pcd[:, 0:2].reshape(pcd.shape[0], 2, 1)).squeeze(-1)
             fig_bev = go.Figure()
-            map_utils.draw_map_plotly("/home/jqian/datasets/boreas-devkit/boreas_mini_v2/boreas_lane.osm", fig_bev, lidar_scan.pose[0, 3], lidar_scan.pose[1, 3], C_a_enu, utm=True)
+            map_utils.draw_map_plotly("/home/shichen/datasets/boreas_mini/boreas_lane.osm", fig_bev, lidar_scan.pose[0, 3], lidar_scan.pose[1, 3], C_a_enu, utm=True)
             fig_bev.add_trace(
                 go.Scattergl(x=pcd_a[:,0], y=pcd_a[:,1], mode='markers', visible=True, marker_size=0.5, marker_color='blue')
             )
@@ -113,8 +111,17 @@ class BoreasPlotly:
             fig_bev.update_xaxes(range=[-75, 75])
             fig_bev.update_yaxes(range=[-75, 75])
 
+            return fig_bev
+
+
+        @app.callback(
+            Output('persp_plot', 'figure'),
+            Input('timestep_slider', 'value')
+        )
+        def update_persp(idx):
             # Perspective
             # Project lidar points into pixel frame
+            pcd, lidar_scan, C_a_enu, C_a_l = self.get_pcd(idx, 0.5)
             pcd = pcd.T
             pcd = np.vstack((pcd, np.ones(pcd.shape[1])))
             points_camera = np.matmul(self.calib.T_camera_lidar, pcd)
@@ -193,9 +200,16 @@ class BoreasPlotly:
                 showlegend=False
             )
 
+            return fig_persp
+
+        @app.callback(
+            Output('radar_plot', 'figure'),
+            Input('timestep_slider', 'value')
+        )
+        def update_radar(idx):
             # Radar
             fig_radar = go.Figure()
-            grid_size = 500
+            grid_size = 600
             grid_res = 0.5
             radar_image = self.get_radar(idx, grid_size, grid_res)
 
@@ -237,38 +251,62 @@ class BoreasPlotly:
                 showlegend=False
             )
 
+            return fig_radar
 
-            
+        @app.callback(
+            Output('color_lidar_plot', 'figure'),
+            Input('timestep_slider', 'value')
+        )
+        def update_color_lidar(idx):
+            # Colored lidar
+            pcd, lidar_scan, C_a_enu, C_a_l = self.get_pcd(idx, 0.5)
+            pcd = pcd.T
+            pcd = np.vstack((pcd, np.ones(pcd.shape[1])))
+            points_camera_f = np.matmul(self.calib.T_camera_lidar, pcd)
+            points_camera = np.matmul(self.calib.P0, points_camera_f)
+            pixel_camera = np.divide(points_camera, points_camera[2, :])
+            # Only select valid lidar points
+            valid_pixel_idx = (points_camera[2, :] > 0) & (pixel_camera[1, :] > 0) & (pixel_camera[1, :] < 2048) & (pixel_camera[0, :] > 0) & (pixel_camera[0, :] < 2448)
+            valid_pixel_x = pixel_camera[0][valid_pixel_idx]
+            valid_pixel_y = pixel_camera[1][valid_pixel_idx]
+            valid_pixel_z = points_camera[2][valid_pixel_idx]
 
+            fig_color_lidar = go.Figure()
 
-            # # Colored lidar
-            # fig_colored_lidar = deepcopy(go.Figure())
-            # transform = deepcopy(self.calib.T_camera_lidar)
-            # print(transform)
-            # pseudo_image = 255*np.ones(image.shape)
-            #
-            # points_camera = np.matmul(self.calib.T_camera_lidar, pcd)
-            # pixel_pseudo_camera = np.matmul(self.calib.P0, np.matmul(transform, colorizable_points[0:4,:]))
-            #
-            # for i in range(pixel_pseudo_camera.shape[1]):
-            #     z = pixel_pseudo_camera[2, i]
-            #     x = int(pixel_pseudo_camera[0, i] / z)
-            #     y = int(pixel_pseudo_camera[1, i] / z)
-            #     if z > 0 and x > 0 and x < image.shape[1] and y > 0 and y < image.shape[0]:
-            #         cv2.circle(pseudo_image, (x, y), 2, colorizable_points[4:, i], 2)
-            #
-            # fig_colored_lidar = px.imshow(pseudo_image)
-            # fig_colored_lidar.update_layout(
-            #     autosize=False,
-            #     height=500
-            # )
+            image = self.get_cam(idx)
 
-            return fig_bev, fig_persp, fig_radar
+            colors = image[valid_pixel_y.astype(int), valid_pixel_x.astype(int)]
+            colors_str = [f'rgb({colors[i][0]}, {colors[i][1]}, {colors[i][2]})' for i in range(colors.shape[0])]
+
+            fig_color_lidar.add_trace(
+                go.Scatter3d(
+                    x=valid_pixel_x,
+                    y=valid_pixel_y,
+                    z=valid_pixel_z,
+                    mode='markers',
+                    marker_size=2,
+                    marker_color=colors_str,
+                )
+            )
+
+            camera = dict(
+                up=dict(x=0, y=-3, z=0),
+                center=dict(x=0, y=0, z=0),
+                eye=dict(x=0.1, y=-0.1, z=-2)
+            )
+            fig_color_lidar.update_layout(
+                scene_camera=camera,
+                width=1200,
+                height=600,
+                autosize=False,
+            )
+
+            return fig_color_lidar
         
         @app.callback(
-            Output('timestep-slider', 'value'),
+            Output('timestep_slider', 'value'),
             Input('animator', 'n_intervals'),
-            State('timestep-slider', 'value'),
+            State('timestep_slider', 'value'),
         )
         def on_click(n_intervals, slider_idx):
             if n_intervals is None:
